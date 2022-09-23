579897484f7c41ca3788555b678ee7e4353ed146
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-09-24 00:26:42 +0200

    config: state management with redux for the whole app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,3 +43,84 @@ if (typeof window !== "undefined") {
     composeEnhancers()
   );
 }
+
+function AppMeta() {
+  
+  useEffect(() => {
+    setUser();
+  });
+  const dispatch = useDispatch();
+
+  const setUser = () => {
+    if (AuthService.isLoggedIn()) {
+      if (!AuthService.tokenExpired()) {
+        const token: any = AuthService.getDecToken();
+        UserService.get(jwt(token).id)
+          .then((res) => {
+            const curr_user = updateJavaScriptObject(jwt(token), res.data);
+            // console.log("Curr user ",curr_user)
+            curr_user.fullNames = res.data.firstName + " " + res.data.lastName;
+            dispatch(setAuthUser(curr_user));
+          })
+          .catch((e) => console.log(e));
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>{app_config.APP_DISCRIPTO}</title>
+        <meta
+          name={"description"}
+          content={
+            "You no longer have to worry about missing treatment from hospitals. You have to go knowing even when you'll be treated"
+          }
+        />
+        <meta property={"og:title"} content={"Medical Appointment System"} />
+        <link rel="icon" href="/favicon_io/favicon.ico" />
+        <meta
+          name="keywords"
+          content="hcm, medical, appointment system, hospital appointment system, appointment treatment"
+        />
+        <meta name="application-name" content={app_config.APP_NAME} />
+        <meta name="author" content="HCM Corporations" />
+        <meta property="og:description" content={app_config.APP_DISCRIPTO} />
+      </Head>
+    </div>
+  );
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    (async function () {
+      try {
+        let detail = await details();
+        if (!sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY)) {
+          sessionStorage.setItem(
+            DEVICE_DETAILS_LOCAL_STORAGE_KEY,
+            JSON.stringify(detail)
+          );
+          try {
+            await SiteTrafficService.create(detail);
+          } catch {
+            (e: any) => console.log(e);
+          }
+        }
+      } catch {
+        (e: any) => console.log(e);
+      }
+    })();
+  }, []);
+  return (
+    <Provider store={store}>
+      <div>
+        <Snack />
+        <AppMeta />
+        <Component {...pageProps} />
+      </div>
+    </Provider>
+  );
+}
+
+export default MyApp;
