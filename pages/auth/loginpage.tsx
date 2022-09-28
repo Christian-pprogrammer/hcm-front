0551e8d6b8cd51392886e0934b4ca521f0475cca
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-28 12:32:23 +0200

    Session Activation

--- pages/auth/loginpage.tsx
+++ pages/auth/loginpage.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import LoginForm from '../../components/Auth/LoginForm'
+import {
+  DEVICE_DETAILS_LOCAL_STORAGE_KEY,
+} from "../../utils/constants";
+import RouteService from '../../services/auth/routing'
+import { useRouter } from 'next/router';
 
 const Login = () => {
+  const Router = useRouter();
+  const SessionRedirect = async () => {
+    if(sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY)){
+      const RouteRedirect = await RouteService.getPrevRoute();
+      return Router.push(`${RouteRedirect}`);
+    }
+  }
   return (
-    <div>
-        <LoginForm/>
-    </div>
+    <>
+    {!sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY)
+        ?  <LoginForm/> : SessionRedirect()}
+    </>
   )
 }
 
