e29dae68d2826fc73cd1e780ee59d0034135f6bb
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-02 21:26:11 +0200

    fix : session

--- pages/auth/loginpage.tsx
+++ pages/auth/loginpage.tsx
@@ -8,39 +8,23 @@ import { useRouter } from 'next/router';
 
 const Login = () => {
   const Router = useRouter();
-<<<<<<< HEAD
   // const SessionRedirect = async () => {
   //   if(sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY)){
-  //     const RouteRedirect = await RouteService.getPrevRoute();
-  //     if(RouteRedirect == null){
-  //       Router.push('/auth/loginpage');
-  //     }
+  //     const RouteRedirect = await RouteService.getPrevRoute() || '/auth/loginpage';
   //     return Router.push(`${RouteRedirect}`);
   //   }
   // }
-  return (
-    <>
-    {/* {!sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY) */}
-        {/* ?  */}
-         <LoginForm/> 
-         {/* : SessionRedirect()} */}
-=======
-  const SessionRedirect = async () => {
-    if(sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY)){
-      const RouteRedirect = await RouteService.getPrevRoute() || '/auth/loginpage';
-      return Router.push(`${RouteRedirect}`);
-    }
-  }
   
-  const [storageKey, setStorageKey]:any = useState(null);
-  useEffect(()=>{
-    setStorageKey(sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY));
-  }, [])
+  // const [storageKey, setStorageKey]:any = useState(null);
+  // useEffect(()=>{
+  //   setStorageKey(sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY));
+  // }, [])
   return (
     <>
-    {!storageKey
-        ?  <LoginForm/> : SessionRedirect()}
->>>>>>> 847e0676290512a718b450116e86915c88247c35
+    {/* {!storageKey */}
+        {/* // ?  */}
+         <LoginForm/>
+        {/* //  : SessionRedirect()} */}
     </>
   )
 }
