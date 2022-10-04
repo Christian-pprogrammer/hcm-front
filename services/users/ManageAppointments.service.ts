02c3f6ecec8ac7d86432dccaab7235c35c84ab10
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-04 10:39:31 +0200

    manage appointments services

---
+++ services/users/ManageAppointments.service.ts
@@ -0,0 +1,34 @@
+import http from "../http-common";
+
+class ManageAppointments{
+    createAppointment(scheduleId:string,data:any){
+        return http.post(`/api/v2/appointments/${scheduleId}`,data)
+    }
+    approveAll(username:string,password:string){
+        return http.put(`/api/v2/appointments/`+username+'/'+password);
+    }
+    approveAppointment(appointmentId: string){
+        return http.put(`/api/v2/appointments/approve/${appointmentId}`,)
+    }
+    changeSchedule(ScheduleDataId:any){
+        return http.patch(`/api/v2/appointments/change-schedule`,ScheduleDataId);
+    }
+    declineAppointment(appointmentId:string){
+        return http.put(`/api/v2/appointments/decline/${appointmentId}`,)
+    }
+    getDoctorAppointments(hospitalId:string,doctorId:string){
+        return http.get(`/api/v2/appointments/hospital/${hospitalId}/doctor/${doctorId}`);
+    }
+    getHospitalAppointments(hospitalId:string){
+        return http.get(`/api/v2/appointments/hospital/${hospitalId}`);
+    }
+    getScheduleAppointments(scheduleId:string){
+        return http.get(`/api/v2/appointments/schedule/${scheduleId}`);
+    }
+    editAppointment(appointmentId:string,scheduleId:string,data:any){
+        return http.patch(`/api/v2/appointments/schedule/${scheduleId}/edit/${appointmentId}`,data);
+    }
+    sendAppointments(data:any) {
+        return http.post(`/api/v2/appointments/send-appointment`,data);
+    }
+}
