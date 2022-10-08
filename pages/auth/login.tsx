12bd70c9e02af596157f9c2370b6e8bb2bdabcfe
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-08 23:19:34 +0200

    Session fixation

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,26 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import LoginForm from '../../components/Auth/LoginForm'
-import {
-  DEVICE_DETAILS_LOCAL_STORAGE_KEY,
-} from "../../utils/constants";
-
-import LoaderCache from './LoaderCache';
 
 const Login = () => {
-  const [showLoaderModal,setShowLoaderModal] = useState(false);
-  const [storageKey, setStorageKey]:any = useState();
-  useEffect(()=>{
-    setStorageKey(sessionStorage.getItem(DEVICE_DETAILS_LOCAL_STORAGE_KEY));
-    setShowLoaderModal(true);
-  }, [])
+
   return (
     <>
-    {!storageKey
-        ? 
-         <LoginForm/>
-         :
-         <LoaderCache showModal={showLoaderModal} onClose={()=>{setShowLoaderModal(false)}}/>
-         }
+      <LoginForm/>
     </>
   )
 }
