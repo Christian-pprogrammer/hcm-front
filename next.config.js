a4c6c76e7e3d18c833e171fdee85ba30cc368c99
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-12 12:03:41 +0200

    Next Internalization config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+  swcMinify: true,
+    i18n: {
+      locales: ['en-US', 'pt-BR'],
+      defaultLocale: 'en-US',
+    },
+  
+    images: {
+      domains: ['avatars.githubusercontent.com'],
+    },
+  
+    poweredByHeader: false,
+  };
+  
+  module.exports = nextConfig;
