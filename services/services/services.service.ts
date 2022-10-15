a685d895eca0c32663ef60c219388bfccc582cbc
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-10-16 01:36:46 +0200

    feat: service and schedule services

---
+++ services/services/services.service.ts
@@ -0,0 +1,32 @@
+import http from "../http-common";
+
+class ManageServices {
+  createService(data: any) {
+    return http.post(`/api/v2/services`, data);
+  }
+
+  getGroupServices(groupId: string) {
+    return http.get(`/api/v2/services/group/${groupId}`);
+  }
+
+  getHospitalServices(hospitalId: string) {
+    return http.get(`/api/v2/services/hospital/${hospitalId}`)
+  }
+
+  addServiceToGroup(groupId: string, data: any) {
+    return http.post(`/api/v2/services/group/${groupId}/add-service`, data);
+  }
+
+  addServiceToHospital(hospitalId: string, data: any) {
+    return http.post(`/api/v2/services/hospital/${hospitalId}`, data);
+  }
+
+  removeAllServicesFromHospital(hospitalId: string) {
+    return http.delete(`/api/v2/services/hospital/${hospitalId}/remove-all`);
+  }
+
+  removeServiceFromHospital(hospitalId: string, serviceId: string) {
+    return http.delete(`/api/v2/services/hospital/${hospitalId}/remove/${serviceId}`);
+  }
+}
+export default new ManageServices();
