7904344a5d4beef04a6d0b7fd6cd8c995c663a3d
Dushime Bill Benon <DushimeBillBenon@gmail.com>
2024-03-12 13:37:36 +0200

    fix: internationalization removed initially

--- components/landingpage/Header.tsx
+++ components/landingpage/Header.tsx
@@ -4,12 +4,10 @@ import { useRouter } from 'next/router';
 
 import { CloseToggle, MenuIcon } from '../../icons'
 import { app_config } from '../../utils/constants';
-import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
 const Header = () => {
     const [ShowLinks, setShowLinks] = useState<Boolean>(false);
-    const trl = useTranslations('Homepage.Navbar');
     const ChangeHeader = (path: string) => {
         const router = useRouter()
         if (router.asPath == path) {
@@ -29,10 +27,10 @@ const Header = () => {
             </div>
             <div className={`flex ${ShowLinks && 'flex flex-col  gap-6 justify-center'}  place-items-center justify-center  md:gap-[2em] lg:gap-[8em]`}>
                 <ul className={`text-black text-[14px] font-normal ${ShowLinks ? 'flex flex-col gap-6 text-center justify-center' : 'hidden'}  md:flex gap-10`}>
-                    <li className={`${ChangeHeader('/')} hover:border-b-2 hover:border-solid hover:border-backG py-2 hover:duration-700 hover:text-blue-500`}><Link href="/">{trl(`links.homedata`)}</Link></li>
-                    <li className={`${ChangeHeader('/#about')} hover:border-b-2 hover:border-solid hover:border-backG py-2 hover:duration-700 hover:text-blue-500`}><Link href="/#about">{trl(`links.about`)}</Link></li>
-                    <li className={`${ChangeHeader('/#services')} hover:border-b-2 hover:border-solid hover:border-backG py-2 hover:duration-700 hover:text-blue-500`}><Link href="/#services">{trl(`links.showcase`)}</Link></li>
-                    <li className={`${ChangeHeader('/patient/landing-appointment-page')} hover:border-b-2 hover:border-solid hover:border-backG py-2 hover:duration-700 hover:text-blue-500`}><Link href="/patient/landing-appointment-page">{trl(`links.appointment`)}</Link></li>
+                    <li className={`${ChangeHeader('/')} hover:border-b-2 hover:border-solid hover:border-backG py-2 hover:duration-700 hover:text-blue-500`}><Link href="/">Home</Link></li>
+                    <li className={`${ChangeHeader('/#about')} hover:border-b-2 hover:border-solid hover:border-backG py-2 hover:duration-700 hover:text-blue-500`}><Link href="/#about">About Us</Link></li>
+                    <li className={`${ChangeHeader('/#services')} hover:border-b-2 hover:border-solid hover:border-backG py-2 hover:duration-700 hover:text-blue-500`}><Link href="/#services">Showcase</Link></li>
+                    <li className={`${ChangeHeader('/patient/landing-appointment-page')} hover:border-b-2 hover:border-solid hover:border-backG py-2 hover:duration-700 hover:text-blue-500`}><Link href="/patient/landing-appointment-page">Appointments</Link></li>
                 </ul>
                 <Link href='/auth/login' className={` ${ShowLinks ? 'flex flex-col gap-6 py-10 justify-center' : 'hidden'}  `}>
                     <button className={`btn lg:flex border-solid hover:bg-backG hover:text-white duration-600 border-backG border-2 rounded-md  justify-center  text-backG py-2 px-10 font-semibold  ${ShowLinks ? 'flex' : 'hidden'}  `}>Login</button>
